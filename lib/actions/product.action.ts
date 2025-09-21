a5a0ec41ea02e0bf029177029b16901f99824825
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-09-21 23:59:11 +0700

    feat: Add custom hook for browsing history with Zustand

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,67 +1,66 @@
-'use server'
+"use server";
 
-import { connectToDatabase } from '@/lib/db'
-import Product, { IProduct } from '@/lib/db/models/product.model'
-import { IProductInput } from '@/types'
-import { PAGE_SIZE } from '../constants'
+import { connectToDatabase } from "@/lib/db";
+import Product, { IProduct } from "@/lib/db/models/product.model";
+import { PAGE_SIZE } from "../constants";
 
 export async function getAllCategories() {
-  await connectToDatabase()
+  await connectToDatabase();
   const categories = await Product.find({ isPublished: true }).distinct(
-    'category'
-  )
-  return categories
+    "category"
+  );
+  return categories;
 }
 export async function getProductsForCard({
   tag,
   limit = 4,
 }: {
-  tag: string
-  limit?: number
+  tag: string;
+  limit?: number;
 }) {
-  await connectToDatabase()
+  await connectToDatabase();
   const products = await Product.find(
     { tags: { $in: [tag] }, isPublished: true },
     {
       name: 1,
-      href: { $concat: ['/product/', '$slug'] },
-      image: { $arrayElemAt: ['$images', 0] },
+      href: { $concat: ["/product/", "$slug"] },
+      image: { $arrayElemAt: ["$images", 0] },
     }
   )
-    .sort({ createdAt: 'desc' })
-    .limit(limit)
+    .sort({ createdAt: "desc" })
+    .limit(limit);
   return JSON.parse(JSON.stringify(products)) as {
-    name: string
-    href: string
-    image: string
-  }[]
+    name: string;
+    href: string;
+    image: string;
+  }[];
 }
 
 export async function getProductsByTag({
   tag,
   limit = 10,
 }: {
-  tag: string
-  limit?: number
+  tag: string;
+  limit?: number;
 }) {
-  await connectToDatabase()
+  await connectToDatabase();
   const products = await Product.find({
     tags: { $in: [tag] },
     isPublished: true,
   })
-    .sort({ createdAt: 'desc' })
-    .limit(limit)
+    .sort({ createdAt: "desc" })
+    .limit(limit);
   // JSON.stringify() serializes only the actual data (plain values).
   // JSON.parse() then recreates a pure JSON object (without Mongoose’s prototype chain)
-  return JSON.parse(JSON.stringify(products)) as IProduct[]
+  return JSON.parse(JSON.stringify(products)) as IProduct[];
 }
 
 // GET ONE PRODUCT BY SLUG
 export async function getProductBySlug(slug: string) {
-  await connectToDatabase()
-  const product = await Product.findOne({ slug, isPublished: true })
-  if (!product) throw new Error('Product not found')
-  return JSON.parse(JSON.stringify(product)) as IProduct
+  await connectToDatabase();
+  const product = await Product.findOne({ slug, isPublished: true });
+  if (!product) throw new Error("Product not found");
+  return JSON.parse(JSON.stringify(product)) as IProduct;
 }
 // GET RELATED PRODUCTS: PRODUCTS WITH SAME CATEGORY
 export async function getRelatedProductsByCategory({
@@ -70,25 +69,25 @@ export async function getRelatedProductsByCategory({
   limit = PAGE_SIZE,
   page = 1,
 }: {
-  category: string
-  productId: string
-  limit?: number
-  page: number
+  category: string;
+  productId: string;
+  limit?: number;
+  page: number;
 }) {
-  await connectToDatabase()
-  const skipAmount = (Number(page) - 1) * limit
+  await connectToDatabase();
+  const skipAmount = (Number(page) - 1) * limit;
   const conditions = {
     isPublished: true,
     category,
     _id: { $ne: productId },
-  }
+  };
   const products = await Product.find(conditions)
-    .sort({ numSales: 'desc' })
+    .sort({ numSales: "desc" })
     .skip(skipAmount)
-    .limit(limit)
-  const productsCount = await Product.countDocuments(conditions)
+    .limit(limit);
+  const productsCount = await Product.countDocuments(conditions);
   return {
     data: JSON.parse(JSON.stringify(products)) as IProduct[],
     totalPages: Math.ceil(productsCount / limit),
-  }
+  };
 }
