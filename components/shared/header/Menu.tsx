e965968333f793ef8c4a95f169a0f9988339f86d
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-09-21 23:59:37 +0700

    feat: Build relative and browsing history of product

--- components/shared/header/Menu.tsx
+++ components/shared/header/Menu.tsx
@@ -1,21 +1,20 @@
-import { ShoppingCartIcon, UserIcon } from 'lucide-react'
-import Link from 'next/link'
+import { ShoppingCartIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Menu() {
   return (
-    <div className='flex justify-end'>
-      <nav className='flex gap-3 w-full'>
-        <Link href='/signin' className='header-button'>
-          {/* <UserIcon className='h-8 w-8' /> */}
-          <span className='align-middle hover:text-white/90'>
+    <div className="flex justify-end">
+      <nav className="flex gap-3 w-full">
+        <Link href="/signin" className="header-button">
+          <span className="align-middle hover:text-white/90">
             Hello, Sign in
           </span>
         </Link>
 
-        <Link href='/cart' className='header-button'>
-          <ShoppingCartIcon className='h-8 w-8' />
+        <Link href="/cart" className="header-button">
+          <ShoppingCartIcon className="h-8 w-8" />
         </Link>
       </nav>
     </div>
-  )
+  );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,24 +1,23 @@
-import { APP_NAME } from '@/lib/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import { MenuIcon } from 'lucide-react'
-import data from '@/lib/data'
-import Search from './Search'
-import Menu from './Menu'
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { MenuIcon } from "lucide-react";
+import data from "@/lib/data";
+import Search from "./Search";
+import Menu from "./Menu";
 
 export default function Header() {
   return (
-    <header className='bg-black  text-white'>
-      <div className='px-2'>
-        <div className='flex items-center justify-between'>
-          <div className='flex items-center'>
+    <header className="bg-black  text-white">
+      <div className="px-2">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
             <Link
-              href='/'
-              className='flex items-center header-button font-bold text-2xl m-1 '
+              href="/"
+              className="flex items-center header-button font-bold text-2xl m-1 "
             >
               <Image
-                src='/icons/logo.png'
+                src="/icons/logo.png"
                 width={40}
                 height={40}
                 alt={`${APP_NAME} logo`}
@@ -26,26 +25,26 @@ export default function Header() {
               {APP_NAME}
             </Link>
           </div>
-          <div className='hidden md:block flex-1 max-w-xl'>
+          <div className="hidden md:block flex-1 max-w-xl">
             <Search />
           </div>
           <Menu />
         </div>
-        <div className='md:hidden block py-2'>
+        <div className="md:hidden block py-2">
           <Search />
         </div>
       </div>
-      <div className='flex items-center px-3 mb-[1px] bg-gray-800'>
-        <div className='dark header-button flex items-center gap-1 text-base [&_svg]:size-6 hover:border-b-white'>
+      <div className="flex items-center px-3 mb-[1px] bg-gray-800">
+        <div className="dark header-button flex items-center gap-1 text-base [&_svg]:size-6 hover:border-b-white">
           <MenuIcon />
           All
         </div>
-        <div className='flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px] '>
+        <div className="flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px] ">
           {data.headerMenus.map((menu) => (
             <Link
               href={menu.href}
               key={menu.href}
-              className='header-button !p-2 hover:border-b hover:border-b-white'
+              className="header-button !p-2 hover:border-b hover:border-b-white"
             >
               {menu.name}
             </Link>
@@ -53,5 +52,5 @@ export default function Header() {
         </div>
       </div>
     </header>
-  )
+  );
 }
