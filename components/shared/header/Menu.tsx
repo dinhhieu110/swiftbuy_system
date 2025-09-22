a7dc5da9b5f726d40a546fced3d97889ea636697
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-09-22 10:49:24 +0700

    feat: Build common component: Add-to-cart

--- components/shared/header/Menu.tsx
+++ components/shared/header/Menu.tsx
@@ -1,20 +1,22 @@
-import { ShoppingCartIcon } from "lucide-react";
-import Link from "next/link";
+import Link from 'next/link'
+import CartButton from './cart-button'
 
 export default function Menu() {
   return (
-    <div className="flex justify-end">
-      <nav className="flex gap-3 w-full">
-        <Link href="/signin" className="header-button">
-          <span className="align-middle hover:text-white/90">
+    <div className='flex justify-end'>
+      <nav className='flex gap-3 w-full'>
+        <Link href='/signin' className='header-button'>
+          <span className='align-middle hover:text-white/90'>
             Hello, Sign in
           </span>
         </Link>
 
-        <Link href="/cart" className="header-button">
-          <ShoppingCartIcon className="h-8 w-8" />
-        </Link>
+        <div className='flex justify-end'>
+          <nav className='flex gap-3 w-full'>
+            <CartButton />
+          </nav>
+        </div>
       </nav>
     </div>
-  );
+  )
 }
